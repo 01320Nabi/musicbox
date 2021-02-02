a3e660c2d1e68542f09be0dfd6676e2ad458bd76
01320Nabi <yangeh2225@gmail.com>
2021-02-02 23:39:37 +0900

    Initial commit

---
+++ playmusicbox.js
@@ -0,0 +1,41 @@
+"use strict"
+function playMusicBox(sheet, type) {
+    let ctx = new AudioContext()
+    const tunes = {
+        "C.": 3,
+        "C#": 4,
+        "Db": 4,
+        "D.": 5,
+        "D#": 6,
+        "Eb": 6,
+        "E.": 7,
+        "F.": 8,
+        "F#": 9,
+        "Gb": 9,
+        "G.": 10,
+        "G#": 11,
+        "Ab": 11,
+        "A.": 12,
+        "A#": 13,
+        "Bb": 13,
+        "B.": 14,
+    }
+    const getFrequency = (tune) => {
+        return 440 * Math.pow(2, tunes[tune.substr(0, 2)] / 12) * Math.pow(2, tune[2]-4)
+    }
+    let speed = sheet.bpm / 60
+    sheet.notes.forEach(note => {
+        let oscillator = ctx.createOscillator()
+        let gain = ctx.createGain()
+        if(getFrequency(note.key)) {
+            console.log(note.key)
+        }
+        oscillator.frequency.value = getFrequency(note.key)
+        oscillator.type = type
+        oscillator.start(speed * (note.msr * sheet.signature * 4 + note.pos))
+        oscillator.stop(speed * (note.msr * sheet.signature * 4 + note.pos + note.dur))
+        gain.gain.setValueCurveAtTime([note.pwr / 25, 0], speed * (note.msr * sheet.signature * 4 + note.pos), 5)
+        oscillator.connect(gain)
+        gain.connect(ctx.destination)
+    });
+}
